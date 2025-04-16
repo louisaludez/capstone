35dda70cbd88eda73f55b54a63c93d70a81e4f1c
Danny-Jtm <danejoelmacarine@gmail.com>
2025-04-17 01:02:27 +0800

    dsd

--- laundryStaff/urls.py
+++ laundryStaff/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.home,name="Home"),
-    path("/orders",views.orders,name="Orders"),
-    path("/messages",views.messages,name="Messages"),
+    path("",views.home,name="HomeLaundryStaff"),
+    path("/orders",views.orders,name="OrdersLaundryStaff"),
+    path("/messages",views.messages,name="MessagesLaundryStaff"),
 ]
