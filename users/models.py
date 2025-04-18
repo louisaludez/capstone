50f461669aea9a0494f46c0f4124a1b30fa96c20
Danny-Jtm <danejoelmacarine@gmail.com>
2025-04-18 17:11:48 +0800

    sdsfs

--- users/models.py
+++ users/models.py
@@ -3,9 +3,25 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     ROLE_CHOICES = [
-        ('staff', 'Staff'),
-        ('laundry', 'Laundry'),
+        ('personnel', 'Personnel'),
         ('admin', 'Admin'),
+        ('manager', 'Manager'),
+
+        # Supervisors
+        ('supervisor_laundry', 'Supervisor - Laundry'),
+        ('supervisor_concierge', 'Supervisor - Concierge'),
+        ('supervisor_cafe', 'Supervisor - Cafe'),
+        ('supervisor_restaurant', 'Supervisor - Restaurant'),
+        ('supervisor_room_service', 'Supervisor - Room Service'),
+        ('supervisor_fnb', 'Supervisor - Food and Beverages'),
+
+        # Staff
+        ('staff_laundry', 'Staff - Laundry'),
+        ('staff_concierge', 'Staff - Concierge'),
+        ('staff_cafe', 'Staff - Cafe'),
+        ('staff_restaurant', 'Staff - Restaurant'),
+        ('staff_room_service', 'Staff - Room Service'),
+        ('staff_fnb', 'Staff - Food and Beverages'),
     ]
 
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='staff')
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES, default='personnel')
