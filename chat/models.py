c6eabbbc427b40ede4f11def17f35bcb3aedcc46
louisaludez <johnlouisaludez@gmail.com>
2025-04-29 06:58:49 +0800

    routing

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Chat(models.Model):
+    sender = models.CharField(max_length=50)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now=True, auto_now_add=True)
+
+    
--- users/views.py
+++ users/views.py
@@ -23,8 +23,27 @@ def login(request):
             elif user.role == 'supervisor_laundry':
                 return redirect('supervisor_laundry_home')
             
-           
+            elif user.role == 'staff_laundry':
+                return redirect('staff_laundry_home')
              # fallback
+            elif user.role == 'supervisor_concierge':
+                return redirect('supervisor_concierge_home')
+            elif user.role == 'supervisor_cafe':
+                return redirect('supervisor_cafe_home')
+            elif user.role == 'supervisor_room_service':
+                return redirect('supervisor_home_service_home')
+            elif user.role == 'supervisor_fnb':
+                return redirect('supervisor_fnb_home')
+            elif user.role == 'staff_concierge':
+                return redirect('staff_concierge_home')
+            elif user.role == 'staff_cafe':
+                return redirect('staff_cafe_home')
+            elif user.role == 'staff_restaurant':
+                return redirect('staff_restaurant_home')
+            elif user.role == 'staff_room_servie':
+                return redirect('staff_room_service_home')
+            elif user.role == 'staff_fnb':
+                return redirect('staff_fnb_home')
         else:
             return render(request, "registration/login.html", {"error": "Invalid credentials"}) 
     return render(request, "registration/login.html")
