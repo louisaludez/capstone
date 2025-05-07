694f74324e81c782e2d8d2d7f3e3eb28689ac6d2
louisaludez <johnlouisaludez@gmail.com>
2025-05-07 23:36:10 +0800

    wownaman

--- cafe/views/staff_views.py
+++ cafe/views/staff_views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def staff_cafe_home(request):
     return render(request,'staff_cafe/home.html')
+def staff_cafe_orders(request):
+    return render(request,'staff_cafe/orders.html')
+def staff_cafe_messages(request):
+    return render(request,'staff_cafe/messages.html')
