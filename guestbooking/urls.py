e9f35bc4416e07f6e30a199a5bf6b9bdf1030aad
louisaludez <johnlouisaludez@gmail.com>
2025-08-18 15:35:46 +0800

    nani

--- guestbooking/urls.py
+++ guestbooking/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', guest_booking_home, name='guest_booking_home'),
+    path('results/', guest_booking_results, name='guest_booking_results'),
 ]
--- guestbooking/views.py
+++ guestbooking/views.py
@@ -3,3 +3,26 @@ from django.shortcuts import render
 # Create your views here.
 def guest_booking_home(request):
     return render(request, 'guestbooking/home.html')
+
+
+def guest_booking_results(request):
+    # Read query params
+    stay_type = request.GET.get('stayType', 'overnight')
+    checkin = request.GET.get('checkin')
+    checkout = request.GET.get('checkout')
+    rooms = request.GET.get('rooms')
+    adults = request.GET.get('adults')
+    children = request.GET.get('children')
+    child_ages = request.GET.get('childAges', '')
+
+    context = {
+        'stay_type': stay_type,
+        'checkin': checkin,
+        'checkout': checkout,
+        'rooms': rooms,
+        'adults': adults,
+        'children': children,
+        'child_ages': child_ages,
+    }
+
+    return render(request, 'guestbooking/results.html', context)
